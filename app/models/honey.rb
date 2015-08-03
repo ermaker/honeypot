b413eef9a234ccd5bbeb5b554ed964a90a5ae077
Minwoo Lee <ermaker@gmail.com>
2015-08-03 21:34:29 +0900

    Add Honey#attributes_without_id

--- app/models/honey.rb
+++ app/models/honey.rb
@@ -5,5 +5,10 @@ module Honeypot
   class Honey
     include Mongoid::Document
     include Mongoid::Attributes::Dynamic
+
+    # @return [Hash] Attributes except '_id'
+    def attributes_without_id
+      attributes.reject { |k, _| k == '_id' }
+    end
   end
 end
--- spec/app/models/honey_spec.rb
+++ spec/app/models/honey_spec.rb
@@ -2,10 +2,28 @@ require 'app'
 require 'models/honey'
 
 RSpec.describe Honeypot::Honey do
+  let(:params) { { 'aaa' => 333, 'bbb' => 444 } }
+
   it 'saves anything' do
-    described_class.new(a: 3, b: 4).save
+    described_class.new(params).save
     expect(described_class.all.size).to eq(1)
-    expect(described_class.first.a).to eq(3)
-    expect(described_class.first.b).to eq(4)
+    expect(described_class.first.aaa).to eq(params['aaa'])
+    expect(described_class.first.bbb).to eq(params['bbb'])
+  end
+
+  describe '#attributes_without_id' do
+    it 'works when empty' do
+      expect(subject.attributes).to be_a(Hash)
+      expect(subject.attributes_without_id).to be_empty
+      expect(subject.attributes_without_id).to be_a(Hash)
+    end
+
+    it 'works when empty' do
+      subject = described_class.new(params)
+      expect(subject.attributes).to be_a(Hash)
+      expect(subject.attributes).to a_hash_including(params)
+      expect(subject.attributes_without_id).to be_a(Hash)
+      expect(subject.attributes_without_id).to match(params)
+    end
   end
 end
