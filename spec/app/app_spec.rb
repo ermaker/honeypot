ea02c4922c76230a96cdaabe3a703cf35ab1a4b5
Minwoo Lee <ermaker@gmail.com>
2015-08-03 20:33:03 +0900

    Remove a swap file

--- spec/app/app_spec.rb
+++ spec/app/app_spec.rb
@@ -12,14 +12,26 @@ RSpec.feature Honeypot::App do
     expect(page).to have_content('Hello, Honeypot!')
   end
 
-  scenario 'get and post /new/raw' do
-    raw = { 'a' => 3, 'b' => 4 }
-    visit '/new/raw'
-    within('#new_honey') do
-      fill_in :raw, with: raw.to_json
-      click_button('')
+  feature 'get and post /new/raw' do
+    given(:valid_input) { { 'a' => 3, 'b' => 4 } }
+
+    scenario 'with valid input' do
+      visit '/new/raw'
+      within('#new_honey') do
+        fill_in :raw, with: valid_input.to_json
+        click_button('')
+      end
+      expect(Honeypot::Honey.all.size).to eq(1)
+      expect(Honeypot::Honey.first.attributes).to a_hash_including(valid_input)
     end
-    expect(Honeypot::Honey.all.size).to eq(1)
-    expect(Honeypot::Honey.first.attributes).to a_hash_including(raw)
+
+    given(:invalid_input_json) { '' }
+    scenario 'with invalid input' do
+      visit '/new/raw'
+      within('#new_honey') do
+        fill_in :raw, with: invalid_input_json
+        expect { click_button('') }.to raise_error(JSON::ParserError)
+      end
+    end
   end
 end
