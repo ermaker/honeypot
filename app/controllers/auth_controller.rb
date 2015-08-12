2b0c99dae21a4db50d5d286d29983cc5e9ad3eda
Minwoo Lee <ermaker@gmail.com>
2015-08-12 19:02:56 +0900

    Add auth#start

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,2 +1,8 @@
+# The controller for auth-related actions of Pushbullet
 class AuthController < ApplicationController
+  before_action :authenticate_user!
+
+  def start
+    redirect_to 'http://google.com'
+  end
 end
--- spec/controllers/auth_controller_spec.rb
+++ spec/controllers/auth_controller_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe AuthController do
+  describe 'GET #start' do
+    it 'works without sign-in' do
+      get :start
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it 'works with sign-in' do
+      @user = User.create!(email: 'a@a.com', password: '12345678')
+      sign_in @user
+      get :start
+      expect(response).to redirect_to('http://google.com')
+    end
+  end
 end
