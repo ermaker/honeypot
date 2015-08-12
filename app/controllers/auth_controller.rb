0ab0e7cbe2208a92182721dcaeeedd99efed20af
Minwoo Lee <ermaker@gmail.com>
2015-08-12 20:43:01 +0900

    Modify auth#start

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,17 @@
 class AuthController < ApplicationController
   before_action :authenticate_user!
 
+  def pushbullet_authorize_uri
+    URI('https://www.pushbullet.com/authorize').tap do |uri|
+      uri.query = {
+        client_id: ENV['PUSHBULLET_CLIENT_ID'],
+        redirect_uri: request.base_url + auth_complete_path,
+        response_type: :code
+      }.to_query
+    end.to_s
+  end
+
   def start
-    redirect_to 'http://google.com'
+    redirect_to(pushbullet_authorize_uri)
   end
 end
--- spec/controllers/auth_controller_spec.rb
+++ spec/controllers/auth_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe AuthController do
+  it '#pushbullet_authorize_uri works' do
+    uri = subject.pushbullet_authorize_uri
+    expect(uri).to be_start_with('https://www.pushbullet.com/authorize')
+  end
+
   describe 'GET #start' do
     it 'works without sign-in' do
       get :start
@@ -11,7 +16,7 @@ RSpec.describe AuthController do
       @user = User.create!(email: 'a@a.com', password: '12345678')
       sign_in @user
       get :start
-      expect(response).to redirect_to('http://google.com')
+      expect(response).to redirect_to(subject.pushbullet_authorize_uri)
     end
   end
 end
