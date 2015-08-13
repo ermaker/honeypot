1baf8e967c396531a795d101a6eb7d513568565b
Minwoo Lee <ermaker@gmail.com>
2015-08-13 18:13:54 +0900

    Add SWCertWorker

---
+++ lib/honeypot/sw_cert_worker.rb
@@ -0,0 +1,27 @@
+module Honeypot
+  # The worker class for SW Cert
+  class SWCertWorker
+    include Workerable
+
+    def critera
+      Log.where(type: 'sw_cert')
+    end
+
+    def to_hash(log)
+      Hash[log[:status].map { |l| [l[0...-1], l[-1][/\d+$/].to_i] }]
+    end
+
+    def notify(_diff)
+      User.all.each do |_user|
+      end
+    end
+
+    def check(prev, now)
+      prev = to_hash(prev)
+      now = to_hash(now)
+      now.reject { |k, v| v == prev[k] }.tap do |diff|
+        notify(diff)
+      end
+    end
+  end
+end
