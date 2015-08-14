fbb935c747a6c34abec4a8409b4d9a525bfa62da
Minwoo Lee <ermaker@gmail.com>
2015-08-15 00:02:45 +0900

    Check Log.recent

--- app/models/log.rb
+++ app/models/log.rb
@@ -5,7 +5,7 @@ class Log
   include Mongoid::Timestamps::Created::Short
   include Tailable
 
-  def self.recent(*where)
-    where(*where).desc('c_at').limit(1)
+  def self.recent(where)
+    where(where).desc('c_at').limit(1)
   end
 end
--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -4,12 +4,18 @@ require 'models/concerns/tailable_spec'
 RSpec.describe Log do
   it_behaves_like 'tailable'
 
-  it '#recent works' do
-    create(:log_sw_cert, correct: false)
-    create(:log_sw_cert, correct: true)
-    create(:log, correct: false)
-    actual = described_class.recent(type: :sw_cert).to_a
-    expect(actual.size).to eq(1)
-    expect(actual.first.correct).to be_truthy
+  describe '#recent' do
+    it 'works' do
+      create(:log_sw_cert, correct: false)
+      create(:log_sw_cert, correct: true)
+      create(:log, correct: false)
+      actual = described_class.recent(type: :sw_cert).to_a
+      expect(actual.size).to eq(1)
+      expect(actual.first.correct).to be_truthy
+    end
+
+    it 'works without logs' do
+      expect(described_class.recent(type: :sw_cert).to_a).to be_empty
+    end
   end
 end
