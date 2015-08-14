8c56128da670c5fe61a977404ee05aa67c40d742
Minwoo Lee <ermaker@gmail.com>
2015-08-14 23:12:08 +0900

    Add User factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    email 'a@a.com'
+    password '12345678'
+    token_type 'Bearer'
+    access_token 'o.JpVq5V1AWSYRynNkAx6QluAxHpV5cuah'
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  subject do
-    User.create!(
-      email: 'a@a.com',
-      password: '12345678',
-      token_type: :Bearer,
-      access_token: 'o.JpVq5V1AWSYRynNkAx6QluAxHpV5cuah'
-    )
-  end
-
+  subject { create(described_class) }
   it '#push works' do
     FakeWeb.register_uri(
       :post,
