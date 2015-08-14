999b3a7eccf25eecd268749887b54f56974c194d
Minwoo Lee <ermaker@gmail.com>
2015-08-14 23:13:47 +0900

    Refactoring

--- spec/controllers/logs_controller_spec.rb
+++ spec/controllers/logs_controller_spec.rb
@@ -18,7 +18,7 @@ require 'rails_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-RSpec.describe LogsController, type: :controller do
+RSpec.describe LogsController do
   # This should return the minimal set of attributes required to create a valid
   # Log. As you add validations to Log, be sure to
   # adjust the attributes here as well.
