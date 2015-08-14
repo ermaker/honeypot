9629638909b05172534775aff4664e376484bcdf
Minwoo Lee <ermaker@gmail.com>
2015-08-14 23:41:03 +0900

    Add #edit_sw_cert

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -1,2 +1,5 @@
+# The setting controller
 class SettingController < ApplicationController
+  def edit_sw_cert
+  end
 end
--- spec/controllers/setting_controller_spec.rb
+++ spec/controllers/setting_controller_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SettingController do
+  describe 'GET #edit_sw_cert' do
+    it 'works' do
+      get :edit_sw_cert
+      expect(response).to be_success
+    end
+  end
 end
