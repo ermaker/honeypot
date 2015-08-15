1bf8408f52726c765254cc8181a02a48bc387e33
Minwoo Lee <ermaker@gmail.com>
2015-08-15 18:30:17 +0900

    Fix for rubocop

--- spec/controllers/setting_controller_spec.rb
+++ spec/controllers/setting_controller_spec.rb
@@ -18,7 +18,9 @@ RSpec.describe SettingController do
     it 'works' do
       sign_in create(:user)
       create(:log_sw_cert)
-      put :update_sw_cert, 'user'=>{'setting'=>['2015년 9월 12 Professional 검정   13:30 ~ 17:30 ( C/C++/JAVA ) 인재개발원(서천)', '2015년 9월 19 Advanced 검정   13:30 ~ 16:30 ( C/C++/JAVA ) 스마트시티(구미)']}
+      # rubocop:disable Metrics/LineLength
+      put :update_sw_cert, 'user' => { 'setting' => ['2015년 9월 12 Professional 검정   13:30 ~ 17:30 ( C/C++/JAVA ) 인재개발원(서천)', '2015년 9월 19 Advanced 검정   13:30 ~ 16:30 ( C/C++/JAVA ) 스마트시티(구미)'] }
+      # rubocop:enable Metrics/LineLength
       expect(response).to redirect_to(:setting_sw_cert)
     end
   end
