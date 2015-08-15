4cee5ff5adfea77978ac848813228462b142319d
Minwoo Lee <ermaker@gmail.com>
2015-08-15 16:26:05 +0900

    Refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  subject { create(described_class) }
   it '#push works' do
     FakeWeb.register_uri(
       :post,
@@ -10,7 +9,7 @@ RSpec.describe User do
       body: { id: :id }.to_json
     )
 
-    result = subject.push(
+    result = create(described_class).push(
       type: :note,
       title: 'title',
       body: 'body'
