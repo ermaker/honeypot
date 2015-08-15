e8d7d50b9e26f5401ed5e8cc8a886aa5e0332e6b
Minwoo Lee <ermaker@gmail.com>
2015-08-15 19:08:46 +0900

    Modify SWCertWorker#notify

--- lib/honeypot/sw_cert_worker.rb
+++ lib/honeypot/sw_cert_worker.rb
@@ -13,21 +13,25 @@ module Honeypot
       Hash[log[:status].map { |l| [l[0...-1], l[-1][/\d+$/].to_i] }]
     end
 
-    def notify(diff)
-      User.all.each do |user|
+    def select_by_user(hash, user)
+      hash.select { |k, _| user.sw_cert_setting.include?(k) }
+    end
+
+    def notify(user, prev, now)
+      prev = select_by_user(to_hash(prev), user)
+      now = select_by_user(to_hash(now), user)
+      now.reject { |k, v| v == prev[k] }.tap do |diff|
         user.push(
           type: :note,
           title: diff.values.sum,
-          body: diff.map { |k, v| "#{k}: #{v}" }.join("\n")
+          body: diff.map { |k, v| "#{k}: #{prev[k]} -> #{v}" }.join("\n")
         )
       end
     end
 
     def check(prev, now)
-      prev = to_hash(prev)
-      now = to_hash(now)
-      now.reject { |k, v| v == prev[k] }.tap do |diff|
-        notify(diff)
+      User.all.each do |user|
+        notify(user, prev, now)
       end
     end
   end
--- spec/lib/honeypot/sw_cert_worker_spec.rb
+++ spec/lib/honeypot/sw_cert_worker_spec.rb
@@ -17,35 +17,27 @@ RSpec.describe Honeypot::SWCertWorker do
         ['2015년 9월', '19', 'Advanced 검정   13:30 ~ 16:30 ( C/C++/JAVA )', '스마트시티(구미)', '잔여석 : 0']
       ] },
       { status: [
-        ['2015년 8월', '22', 'Advanced 검정   09:00 ~ 12:00 ( C/C++/JAVA )', '인재개발원(서천)', '잔여석 : 0'],
-        ['2015년 8월', '22', 'Advanced 검정   09:00 ~ 12:00 ( C/C++/JAVA )', '첨단기술연수소(영통) (외국인 전용)', '잔여석 : 0'],
+        ['2015년 8월', '22', 'Advanced 검정   09:00 ~ 12:00 ( C/C++/JAVA )', '인재개발원(서천)', '잔여석 : 1'],
+        ['2015년 8월', '22', 'Advanced 검정   09:00 ~ 12:00 ( C/C++/JAVA )', '첨단기술연수소(영통) (외국인 전용)', '잔여석 : 1'],
         ['2015년 8월', '29', 'Advanced 검정   14:30 ~ 17:30 ( C/C++/JAVA )', '첨단기술연수소(영통)', '잔여석 : 0'],
         ['2015년 8월', '29', 'Professional 검정   09:00 ~ 13:00 ( C/C++/JAVA )', '첨단기술연수소(영통)', '잔여석 : 1'],
-        ['2015년 8월', '29', 'Professional 검정   09:00 ~ 13:00 ( C/C++/JAVA )', '첨단기술연수소(영통) (외국인 전용)', '잔여석 : 0'],
-        ['2015년 9월', '12', 'Advanced 검정   09:00 ~ 12:00 ( C/C++/JAVA )', '인재개발원(서천)', '잔여석 : 0'],
-        ['2015년 9월', '12', 'Professional 검정   13:30 ~ 17:30 ( C/C++/JAVA )', '인재개발원(서천)', '잔여석 : 0'],
-        ['2015년 9월', '19', 'Advanced 검정   13:30 ~ 16:30 ( C/C++/JAVA )', '스마트시티(구미)', '잔여석 : 0']
+        ['2015년 8월', '29', 'Professional 검정   09:00 ~ 13:00 ( C/C++/JAVA )', '첨단기술연수소(영통) (외국인 전용)', '잔여석 : 1'],
+        ['2015년 9월', '12', 'Advanced 검정   09:00 ~ 12:00 ( C/C++/JAVA )', '인재개발원(서천)', '잔여석 : 1'],
+        ['2015년 9월', '12', 'Professional 검정   13:30 ~ 17:30 ( C/C++/JAVA )', '인재개발원(서천)', '잔여석 : 1'],
+        ['2015년 9월', '19', 'Advanced 검정   13:30 ~ 16:30 ( C/C++/JAVA )', '스마트시티(구미)', '잔여석 : 1']
       ] }
       # rubocop:enable Metrics/LineLength
     ].map do |log|
       create(:log_sw_cert, log)
     end
   end
-  let(:diff) do
-    {
-      [
-        '2015년 8월', '29',
-        'Professional 검정   09:00 ~ 13:00 ( C/C++/JAVA )',
-        '첨단기술연수소(영통)'
-      ] => 1
-    }
-  end
 
   it '#check works' do
-    expect(subject).to receive(:notify).with(Hash).exactly(3).times
-    expect(subject.check(logs[0], logs[0])).to be_empty
-    expect(subject.check(logs[0], logs[1])).to eq(diff)
-    expect(subject.check(logs[1], logs[1])).to be_empty
+    create(:user)
+    expect(subject).to receive(:notify).with(User, Log, Log).exactly(3).times
+    subject.check(logs[0], logs[0])
+    subject.check(logs[0], logs[1])
+    subject.check(logs[1], logs[1])
   end
 
   it '#critera works' do
@@ -53,6 +45,20 @@ RSpec.describe Honeypot::SWCertWorker do
   end
 
   it '#notify works' do
-    skip('TODO')
+    FakeWeb.register_uri(
+      :post,
+      %r{/shards$},
+      content_type: 'application/json',
+      body: { id: :id }.to_json
+    )
+
+    # rubocop:disable Metrics/LineLength
+    expect(subject.notify(create(:user), logs[0], logs[1])).to eq(
+      ['2015년 8월', '22', 'Advanced 검정   09:00 ~ 12:00 ( C/C++/JAVA )', '인재개발원(서천)'] => 1,
+      ['2015년 8월', '29', 'Professional 검정   09:00 ~ 13:00 ( C/C++/JAVA )', '첨단기술연수소(영통)'] => 1,
+      ['2015년 9월', '12', 'Advanced 검정   09:00 ~ 12:00 ( C/C++/JAVA )', '인재개발원(서천)'] => 1,
+      ['2015년 9월', '12', 'Professional 검정   13:30 ~ 17:30 ( C/C++/JAVA )', '인재개발원(서천)'] => 1
+    )
+    # rubocop:enable Metrics/LineLength
   end
 end
