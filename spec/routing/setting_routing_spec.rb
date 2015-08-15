97ad7ac8642eb348ebbc368767b57ddf98f1fdef
Minwoo Lee <ermaker@gmail.com>
2015-08-15 18:27:15 +0900

    Set routes and views

--- spec/routing/setting_routing_spec.rb
+++ spec/routing/setting_routing_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'routing to setting' do
   end
 
   it 'routes /setting/sw_cert to setting#update_sw_cert' do
-    expect(put: '/setting/sw_cert').to route_to(
+    expect(patch: '/setting/sw_cert').to route_to(
       controller: 'setting',
       action: 'update_sw_cert'
     )
