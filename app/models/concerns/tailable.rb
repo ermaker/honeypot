6b931c52c578541344496ab709be8c6889581ec6
Minwoo Lee <ermaker@gmail.com>
2015-08-20 20:26:56 +0900

    Fix rubocop

--- app/models/concerns/tailable.rb
+++ app/models/concerns/tailable.rb
@@ -3,10 +3,9 @@ module Tailable
   extend ActiveSupport::Concern
 
   module ClassMethods # rubocop:disable Style/Documentation
-    def create_collection_tailable!
+    def create_collection_tailable! # rubocop:disable Metrics/AbcSize
       mongo_session.command(
-        create: collection_name.to_s,
-        capped: true,
+        create: collection_name.to_s, capped: true,
         size: 20 * 1024 * 1024
       ).tap { create }
     rescue Moped::Errors::OperationFailure => e
