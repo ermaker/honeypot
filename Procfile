01f9af1660a00794ccd05964e8332aa55ae82237
Minwoo Lee <ermaker@gmail.com>
2016-07-15 16:22:51 +0900

    WIP: swcert

--- Procfile
+++ Procfile
@@ -2,3 +2,4 @@ web: bundle exec puma -C config/puma.rb
 worker_sw_cert: bundle exec rake jobs:worker:sw_cert
 worker_get_student: bundle exec rake jobs:worker:get_student
 worker_center: bundle exec rake jobs:worker:center
+worker_swcert: bundle exec rake jobs:worker:swcert
---
+++ lib/honeypot/worker/swcert.rb
@@ -0,0 +1,58 @@
+require 'honeypot/workable'
+
+module Honeypot
+  module Worker
+    # SWCert Worker
+    class SWCert
+      include Workable
+
+      def critera
+        Log.where(type: 'swcert')
+      end
+
+      def filter(status)
+        status[:status].select { |item| item['CNT4'] < item['PERSON_CNT'] }
+      end
+
+      def to_inspect(item)
+        "#{item['CERTI_NM']}#{item['FOREIGNER_YN'] == 'Y' ? '(Foreigner)'} " \
+          ": #{item['PERSON_CNT'] - item['CNT4']} " \
+          "[#{item['QUAL_DT']} #{item['EDU_PLACE_NM']} " \
+          "#{item['QUAL_ST_TIME']} ~ #{item['QUAL_ED_TIME']} " \
+          "#{item['LANGUAGE_TYPE']}]"
+      end
+
+      def notify(prev, now)
+        MShard::MShard.new.set_safe(
+          slack: true,
+          webhook_url: ENV['SLACK_WEBHOOK_URI'],
+          # channel: '#center',
+          attachments: [
+            {
+              fallback: "SWCERT TEST",
+              pretext: "SWCERT TEST <!channel>",
+              title: "TITLE",
+              text: filter(now).map { |item| to_inspect(item) },
+              footer: "FOOTER"
+            }
+          ],
+        )
+      end
+
+      def check(prev, now)
+        Logger.new($stderr).debug do
+          "[#{prev[:status] == now[:status] ? 'Same' : 'Different'}]"
+        end
+        notify(prev, now) unless prev[:status] == now[:status]
+      end
+
+      def peek_
+        critera.order(id: :desc).limit(2).to_a.reverse
+      end
+
+      def peek
+        notify(*peek_)
+      end
+    end
+  end
+end
